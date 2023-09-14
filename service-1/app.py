5a7f95df42dc644d485de157abe7587562f955a5
JamJarLid <espen.lever@gmail.com>
2023-09-14 11:31:22 +0200

    implement logging

--- service-1/app.py
+++ service-1/app.py
@@ -1,18 +1,61 @@
-from flask import Flask
+import json
+import sys
+import logging
+from flask import Flask, request
 from prometheus_flask_exporter import PrometheusMetrics
 
+class StructuredFormatter(logging.Formatter):
+  """
+  Structure log entries as JSON objects and add request context if available
+  """
+
+  def format(self, record):
+    log_entry= {
+      "path": request.path,
+      "message": request.getMessage(),
+      "timestamp": self.formatTime(record),
+    }
+    if request:
+      log_entry.update(
+        {
+          "path": request.path,
+          "method": request.method,
+          "remote_addr": request.remote_addr,
+        }
+      )
+    if record.exc_info:
+      log_entry["exception"] = self.formatException(record.exc_info)
+    return json.dumps(log_entry)
+
 app = Flask(__name__)
-metrics = PrometheusMetrics(app, path="/metrics")
 
-metrics.info("app_info", "service1", version="1.0.0")
+# Set up our logging handler to use StructuredFormatter
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(StructuredFormatter())
 
+# Add the handler to the Flask app's logger
+app.logger.addHandler(handler)
+app.logger.setLevel(logging.INFO)
+
+# Also change the werkzeug logger, the default Flask logger, to use StructuredFormatter
+logging.getLogger("werkzeug").addHandler(handler)
+
+# Add Prometheus WSGI middleware to track metrics
+metrics = PrometheusMetrics(app)
+
+# Add service metadata for Prometheus
+metrics.info("app_info", "Service 1", version="1.0.0")
+
+# Add a metric to track requests by method
 @metrics.counter(
-    "invocation_by_method",
-    "Number of invocations by HTTP method",
+    "requests_by_method",
+    "Number of requests per method",
+    labels={"method": lambda r: r.method},
 )
 
 @app.route('/')
-def hello():
+def hello_world():
+  app.logger.info("Service 1 called")
   return 'Hello World!'
 
 if __name__ == '__main__':
--- service-2/app.py
+++ service-2/app.py
@@ -1,19 +1,22 @@
 from flask import Flask
 from prometheus_flask_exporter import PrometheusMetrics
 import requests
+import logging
+import logging.handlers
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+logger.info("Service 2 started")
 
 app = Flask(__name__)
-metrics = PrometheusMetrics(app, path="/metrics")
+metrics = PrometheusMetrics(app)
 
-metrics.info("app_info", "service1", version="1.0.0")
-
-@metrics.counter(
-    "invocation_by_method",
-    "Number of invocations by HTTP method",
-)
+metrics.info("app_info", "Service 2", version="1.0.0")
 
 @app.route("/")
-def hello_world():
+def main():
+    logger.info("Service 2 called")
     r = requests.get("http://service1:5001")
     return f"Hello from Service 2. Service 1 says: {r.text}!"
 
